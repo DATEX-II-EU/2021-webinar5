3c1bd844be3526a39fb8e44511678d24b2242672
Jonas Jäderberg <jonas@jaderb.se>
2021-05-21 10:53:31 +0200

    Initial version

---
+++ Demo DotNetCore/DemoSoapServer/Data/DataManager.cs
@@ -0,0 +1,36 @@
+﻿using DemoSoapServer.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DemoSoapServer.Data
+{
+    public class DataManager
+    {
+    
+    public MessageContainer GetData()
+    {
+            MessageContainer data = null;
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = httpClient.GetAsync("http://datex.vegagerdin.is/situationpublication3_1/SituationService/pullsnapshotdata").Result)
+                {
+                    string apiResponse = response.Content.ReadAsStringAsync().Result;
+
+                    XmlSerializer serializer = new XmlSerializer(typeof(MessageContainer));
+                    StringReader reader = new StringReader(apiResponse);
+                    data = (MessageContainer)serializer.Deserialize(reader);
+
+                }
+            }
+
+            return data;
+
+    }
+
+    }
+}
